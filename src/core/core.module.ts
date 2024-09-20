55989d9d805797f70fa5fdcdff03996b2a6050fe
Charly Ramirez <pingestudio@gmail.com>
2024-09-19 20:06:58 -0600

    Agregando configuracion

---
+++ src/core/core.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { UsersModule } from './users/users.module';
+import { RestaurantsModule } from './restaurants/restaurants.module';
+import { PeopleModule } from './people/people.module';
+
+@Module({
+  imports: [UsersModule, RestaurantsModule, PeopleModule]
+})
+export class CoreModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+
+// App
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+
+const logger: Logger = new Logger('PetFrendly');
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
+    logger: ['debug', 'error', 'warn'],
+  });
+
+  logger.debug(`${AppModule.host}:${AppModule.port}`);
+  await app.listen(AppModule.port, AppModule.host);
 }
 bootstrap();
